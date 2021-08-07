dd08e91a29fb0c1ef1b4f7addc850bb218e39e72
Caroline <carolinesalibc@gmail.com>
2021-08-07 11:07:28 -0600

    Add prefix on site name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def set_site_prefix
+    @title_prefix = "#{params["controller"].capitalize} | "
+  end
 end
--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,4 +1,6 @@
 class BlogController < ApplicationController
+  before_action :set_site_prefix
+
   def index
   end
 end
