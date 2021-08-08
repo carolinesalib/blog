bbb666b4329743c06465144dd59d1a4fa2c715d3
Caroline <carolinesalibc@gmail.com>
2021-08-08 15:55:22 -0600

    Display markdown article

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,3 +1,5 @@
+require 'redcarpet'
+
 class BlogController < ApplicationController
   before_action :set_site_prefix
 
@@ -5,8 +7,19 @@ class BlogController < ApplicationController
     articles_yml_result = YAML.load_file("app/articles/articles.yml")["articles"]
     @articles = Article.build_list_of_articles(articles_yml_result)
 
-    if params["tag"].present?
-      @articles = @articles.filter { |article| article.tags.include?(params["tag"]) }
+    if params[:tag].present?
+      @articles = @articles.filter { |article| article.tags.include?(params[:tag]) }
     end
+  end
+
+  def show
+    # TODO: deal with possible errors
+    # TODO: highlight code
+    # TODO: Allow to add images (see how I did that with jykill)
+    # TODO: Test mobile
+    article_content = File.open("app/articles/#{params[:id]}.md").read
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+
+    @article_content_html = markdown.render(article_content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "blog", to: "blog#index"
+  get "blog/:id", to: "blog#show"
   get "about", to: "home#index"
 
   root "home#index"
